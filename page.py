8ba90f8d2b658a07aa627505709949d47dfb35ab
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-22 22:37:12 +0000

    fixing error around markdown table

--- page.py
+++ page.py
@@ -23,24 +23,25 @@ def app():
             st.text_area('Your text will appear here', value=st.session_state["response"], max_chars=None, key=None)
 
     # Section 2: Check Guidance
+def app():
+    # Section 2: Check Guidance
     if st.session_state["response"]:
         st.header('Check Guidance')
         guidance_input = st.text_area('What does it need to comply with (from Section 1)?', value='', max_chars=None, key=None)
-       
+
         if st.button('Check Compliance'):
             if guidance_input:
                 messages = [
                     {"role": "system", "content": "You are a helpful assistant"},
                     {"role": "user", "name": "Alice", "content": f"{guidance_input}. Check the following text: {st.session_state['response']}"},
                 ]
-                result = openai_utils.send_request_to_openai(messages)
-              
-                # Assuming markdown results are coming from OpenAI API
-                markdown_table = "| Criteria | Score | Commentary |\n| --- | --- | --- |\n"
-                for row in result:
-                    markdown_table += f"| {row['criteria']} | {row['score']} | {row['commentary']} |\n"
-                
-                # Render the markdown table
-                st.markdown(markdown_table)
+                raw_response = openai_utils.send_request_to_openai(messages)
+
+                # Now parsing raw_response as per OpenAI's returned structure
+                # This assumes the 'content' field contains a string that we want to display
+                result = raw_response['choices'][0]['message']['content']
+
+                # Render the assessment
+                st.markdown(result)
             else:
                 st.warning('Please enter guidance and ensure text is generated in Section 1')
