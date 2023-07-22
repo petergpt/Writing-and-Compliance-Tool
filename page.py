25957ef6c5d06707b7fc6932fb4f890e630110e7
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-22 22:19:30 +0000

    fixing error with passing text to compliance section

--- page.py
+++ page.py
@@ -3,13 +3,13 @@ import openai
 from utils import openai_utils
 
 def app():
-    st.title('OpenAI Text Generation and Compliance Checking')
+    st.title('Text Generation and Compliance Checking')
 
     # Section 1: Generate Text
     st.header('Generate Text')
     text_input = st.text_area('What do you want to write?', value='', max_chars=None, key=None)
     tone_input = st.text_input('What style should be written in?', value='', max_chars=None, key=None)
-
+    
     if st.button('Generate Text'):
         if text_input and tone_input:
             messages = [
@@ -18,31 +18,23 @@ def app():
             ]
             response = openai_utils.send_request_to_openai(messages)
             st.text_area('Your text will appear here', value=response, max_chars=None, key=None)
+            
+            # "Check Guidance" button
+            if st.button('Check Guidance'):
+                guidance_input = st.text_area('What does it need to comply with?', value='', max_chars=None, key=None)
+                messages = [
+                    {"role": "system", "content": "You are a helpful assistant"},
+                    {"role": "user", "name": "Alice", "content": f"{guidance_input}. Check the following text: {response}"},
+                ]
+                result = openai_utils.send_request_to_openai(messages)
+                
+                # Assuming markdown results are coming from OpenAI API
+                markdown_table = "| Criteria | Score | Commentary |\n| --- | --- | --- |\n"
+                for row in result:
+                    markdown_table += f"| {row['criteria']} | {row['score']} | {row['commentary']} |\n"
+                
+                # Render the markdown table
+                st.markdown(markdown_table)
+    
         else:
             st.warning('Please enter both outline and tone of voice')
-
-    # Section 2: Check Compliance
-    st.header('Check Guidance')
-    guidance_input = st.text_area('What does it need to comply with (from Section 1)?', value='', max_chars=None, key=None)
-    
-    if st.button('Check Compliance'):
-        if response and guidance_input:
-            messages = [
-                {"role": "system", "content": "You are a helpful assistant"},
-                {"role": "user", "name": "Alice", "content": f"{guidance_input}. Check the following text: {response}"},
-            ]
-            result = openai_utils.send_request_to_openai(messages)
-            
-            # Assuming markdown results are coming from OpenAI API as list of dicts
-            # result_data = [{'criteria': 'Criteria 1', 'score': '4/5', 'commentary': 'Good'}]
-
-            # Convert to markdown table format
-            markdown_table = "| Criteria | Score | Commentary |\n| --- | --- | --- |\n"
-            for row in result:
-                markdown_table += f"| {row['criteria']} | {row['score']} | {row['commentary']} |\n"
-            
-            # Render the markdown table
-            st.markdown(markdown_table)
-            
-        else:
-            st.warning('Please enter guidance and ensure text is generated in Section 1')
