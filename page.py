6543d92565a1da04acdf462bc76acd8196ed8b06
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-22 22:27:18 +0000

    try another approach to text generation and checking

--- page.py
+++ page.py
@@ -1,40 +1,35 @@
 import streamlit as st
-import openai
-from utils import openai_utils
+from streamlit.session_state import get_state
+from ..utils import openai_utils
 
 def app():
-    st.title('Text Generation and Compliance Checking')
+    state = get_state()  # initialize session state
 
     # Section 1: Generate Text
     st.header('Generate Text')
-    text_input = st.text_area('What do you want to write?', value='', max_chars=None, key=None)
-    tone_input = st.text_input('What style should be written in?', value='', max_chars=None, key=None)
-    
+    state.text_input = st.text_area('What do you want to write?', value=state.text_input if state.text_input else '', max_chars=None, key=None)
+    state.tone_input = st.text_input('What style should be written in?', value=state.tone_input if state.tone_input else '', max_chars=None, key=None)
+
     if st.button('Generate Text'):
-        if text_input and tone_input:
+        if state.text_input and state.tone_input:
+            messages = [
+              {"role": "system", "content": "You are a helpful assistant"},
+              {"role": "user", "name": "Alice", "content": f"{state.tone_input}. {state.text_input}"},
+            ]
+            state.response = openai_utils.send_request_to_openai(messages)
+            st.text_area('Your text will appear here', value=state.response,  max_chars=None, key=None)
+
+    # Section 2: Check Guidance
+    st.header('Check Guidance')
+    if state.response:
+        state.guidance_input = st.text_area('What does it need to comply with?', value=state.guidance_input if state.guidance_input else '', max_chars=None, key=None)
+        if st.button('Check Compliance'):
             messages = [
                 {"role": "system", "content": "You are a helpful assistant"},
-                {"role": "user", "name": "Alice", "content": f"{tone_input}. {text_input}"},
+                {"role": "user", "name": "Alice", "content": f"{state.guidance_input}. Check the following text: {state.response}"},
             ]
-            response = openai_utils.send_request_to_openai(messages)
-            st.text_area('Your text will appear here', value=response, max_chars=None, key=None)
-            
-            # "Check Guidance" button
-            if st.button('Check Guidance'):
-                guidance_input = st.text_area('What does it need to comply with?', value='', max_chars=None, key=None)
-                messages = [
-                    {"role": "system", "content": "You are a helpful assistant"},
-                    {"role": "user", "name": "Alice", "content": f"{guidance_input}. Check the following text: {response}"},
-                ]
-                result = openai_utils.send_request_to_openai(messages)
-                
-                # Assuming markdown results are coming from OpenAI API
-                markdown_table = "| Criteria | Score | Commentary |\n| --- | --- | --- |\n"
-                for row in result:
-                    markdown_table += f"| {row['criteria']} | {row['score']} | {row['commentary']} |\n"
-                
-                # Render the markdown table
-                st.markdown(markdown_table)
-    
-        else:
-            st.warning('Please enter both outline and tone of voice')
+            result = openai_utils.send_request_to_openai(messages)
+            markdown_table = "| Criteria | Score | Commentary |\n| --- | --- | --- |\n"
+            for row in result:
+                markdown_table += f"| {row['criteria']} | {row['score']} | {row['commentary']} |\n"
+            st.markdown(markdown_table)
