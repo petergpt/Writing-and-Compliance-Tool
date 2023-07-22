c6303de853b4a637c521f62d436aae5d0e91a19f
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-22 23:00:46 +0000

    fixing buttons disappearing

--- page.py
+++ page.py
@@ -12,29 +12,29 @@ def app():
     text_input = st.text_area('What do you want to write?', value='', max_chars=None, key=None)
     tone_input = st.text_input('What style should be written in?', value='', max_chars=None, key=None)
 
-if st.button('Generate Text'):
-    if text_input and tone_input:
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant"},
-            {"role": "user", "name": "Alice", "content": f"{tone_input}. {text_input}"},
-        ]
-        response = openai_utils.send_request_to_openai(messages)
-        st.session_state["response"] = response['choices'][0]['message']['content']
-        st.text_area('Your text will appear here', value=st.session_state["response"], max_chars=None, key=None)
-      
+    if st.button('Generate Text', key='button1'):  # Add a unique key for the button
+        if text_input and tone_input:
+            messages = [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "name": "Alice", "content": f"{tone_input}. {text_input}"},
+            ]
+            response = openai_utils.send_request_to_openai(messages)
+            st.session_state["response"] = response['choices'][0]['message']['content']
+            st.text_area('Your text will appear here', value=st.session_state["response"], max_chars=None, key=None)
+
     # Section 2: Check Guidance
     if st.session_state["response"]:
         st.header('Check Guidance')
         guidance_input = st.text_area('What does it need to comply with (from Section 1)?', value='', max_chars=None, key=None)
 
-if st.button('Check Compliance'):
-    if guidance_input:
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant"},
-            {"role": "user", "name": "Alice", "content": f"{guidance_input}. Check the following text: {st.session_state['response']}"},
-        ]
-        response = openai_utils.send_request_to_openai(messages)
-        result = response['choices'][0]['message']['content']  # Extract the content of the first message
-        st.text(result)
-else:
+        if st.button('Check Compliance', key='button2'):  # Add a unique key for the button
+            if guidance_input:
+                messages = [
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "name": "Alice", "content": f"{guidance_input}. Check the following text: {st.session_state['response']}"},
+                ]
+                response = openai_utils.send_request_to_openai(messages)
+                result = response['choices'][0]['message']['content']  # Extract the content of the first message
+                st.text(result)  # Use st.text() instead of st.markdown()
+            else:
                 st.warning('Please enter guidance and ensure text is generated in Section 1')
