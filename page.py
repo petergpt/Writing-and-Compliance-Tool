e52773b39d48a1c6a515e12f3c63e8e82a2a2d80
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 20:43:01 +0000

    bug fix

--- page.py
+++ page.py
@@ -61,29 +61,32 @@ def app():
             else:
                 st.warning('Please enter guidance and ensure text is generated in Section 1')
 
-# Section 3: Test Persona Perception
-if st.session_state["response"]:
-    st.header('Test Persona Perception')
+    # Section 3: Test Persona Perception
+    if st.session_state["response"]:
+        st.header('Test Persona Perception')
 
-    # Dropdown for Questions
-    st.subheader('Questions')
-    questions_option = st.selectbox('Select a set of questions', list(questions.QUESTIONS_OPTIONS.keys()), key='questions')
-    st.session_state["questions_input"] = st.text_area('The following questions will be answered by the persona', value=questions.QUESTIONS_OPTIONS[questions_option], max_chars=None, key='questions_input_widget')
+        # Dropdown for Questions
+        st.subheader('Questions')
+        questions_option = st.selectbox('Select a set of questions', list(QUESTIONS_OPTIONS.keys()), key='questions')
+        st.session_state["questions_input"] = st.text_area('The following questions will be answered by the persona', value=QUESTIONS_OPTIONS[questions_option], max_chars=None, key='questions_input_widget')
 
-    # Dropdown for Persona
-    st.subheader('Persona')
-    persona_option = st.selectbox('Select a persona', list(personas.PERSONAS_OPTIONS.keys()), key='persona')
-    st.session_state["persona_input"] = st.text_area('The following persona will answer the questions', value=personas.PERSONAS_OPTIONS[persona_option], max_chars=None, key='persona_input_widget')
+        # Dropdown for Persona
+        st.subheader('Persona')
+        persona_option = st.selectbox('Select a persona', list(PERSONAS_OPTIONS.keys()), key='persona')
+        st.session_state["persona_input"] = st.text_area('The following persona will answer the questions', value=PERSONAS_OPTIONS[persona_option], max_chars=None, key='persona_input_widget')
 
-    if st.button('Test Persona', key='button3'):  # Add a unique key for the button
-        if st.session_state["questions_input"] and st.session_state["persona_input"]:
-            with st.spinner('Testing persona perception...'):
-                messages = [
-                    {"role": "system", "content": f"You will take on the persona of a {persona_option} and answer the following questions based on the text: {st.session_state['questions_input']}."},
-                    {"role": "user", "content": f"Here is the text to analyze: {st.session_state['response']}"},
-                ]
-                response = openai_utils.send_request_to_openai(messages)
-                result = response['choices'][0]['message']['content']  # Extract the content of the first message
-                st.markdown(result)
-        else:
-            st.warning('Please select a set of questions and a persona, and ensure text is generated in Section 1')
+        if st.button('Test Persona', key='button3'):  # Add a unique key for the button
+            if st.session_state["questions_input"] and st.session_state["persona_input"]:
+                with st.spinner('Testing persona perception...'):
+                    messages = [
+                        {"role": "system", "content": f"You will take on the persona of a {persona_option} and answer the following questions based on the text: {st.session_state['questions_input']}."},
+                        {"role": "user", "content": f"Here is the text to analyze: {st.session_state['response']}"},
+                    ]
+                    response = openai_utils.send_request_to_openai(messages)
+                    result = response['choices'][0]['message']['content']  # Extract the content of the first message
+                    st.markdown(result)
+            else:
+                st.warning('Please select a set of questions and a persona, and ensure text is generated in Section 1')
+
+if __name__ == '__main__':
+    app()
