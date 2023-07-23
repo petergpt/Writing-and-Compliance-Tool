216cfdc0bbaa837df7e05d55d89e26c2e17a9610
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 20:15:00 +0000

    2x

--- openai_utils.py
+++ openai_utils.py
@@ -6,8 +6,8 @@ openai.organization = "org-RgaR5omwSOHKfgH8FjtxRDis"
 #OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 #openai.api_key = "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"
 
-OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 openai.api_key = OPENAI_API_KEY
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 
 def send_request_to_openai(messages):
     
