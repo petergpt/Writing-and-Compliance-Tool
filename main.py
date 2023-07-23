0468d18cc478b967ae5dee454410c8f11e4b0bc9
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 08:39:24 +0000

    remove navigation panel from main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import streamlit as st
 import page
 
-# Page dictionary only includes single page.py
-PAGES = {
-    "Text Generation & Compliance Check": page,
-}
-
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
+# Call the app function directly
 page.app()
