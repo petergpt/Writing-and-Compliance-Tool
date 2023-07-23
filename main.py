155e6c97749ea6e9207d43dd2b30fd7b24ac50c3
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 20:48:59 +0000

    update main.py for 3 pages

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-import page
+import streamlit as st
+import app_generate_text
+import app_check_guidance
+import app_test_persona
 
-# Call the app function directly
-page.app()
+
+def main():
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", ['Generate Text', 'Check Guidance', 'Test Persona'])
+
+    if selection == 'Generate Text':
+        app_generate_text.app()
+    elif selection == 'Check Guidance':
+        app_check_guidance.app()
+    elif selection == 'Test Persona':
+        app_test_persona.app()
+
+
+if __name__ == "__main__":
+    main()
