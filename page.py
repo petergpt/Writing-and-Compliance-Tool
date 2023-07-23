4b59085605f958437606853384c00ac8b14f32e5
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 08:19:49 +0000

    fixing user flow in page.py

--- page.py
+++ page.py
@@ -10,6 +10,8 @@ def app():
         st.session_state["response"] = ""
     if "generated_text" not in st.session_state:
         st.session_state["generated_text"] = ""
+    if "guidance_input" not in st.session_state:
+        st.session_state["guidance_input"] = ""
 
     # Section 1: Generate Text
     st.header('Generate Text')
@@ -19,11 +21,6 @@ def app():
     st.subheader('Tone of Voice')
     tone_option = st.selectbox('Select a tone', list(TONE_OF_VOICE_OPTIONS.keys()), key='tone')
     tone_input = st.text_area('What style should be written in?', value=TONE_OF_VOICE_OPTIONS[tone_option], max_chars=None, key='tone_input')
-
-    # Dropdown for Guidance
-    st.subheader('Guidance')
-    guidance_option = st.selectbox('Select a guidance', list(GUIDANCE_OPTIONS.keys()), key='guidance')
-    guidance_input = st.text_area('What does it need to comply with (from Section 1)?', value=GUIDANCE_OPTIONS[guidance_option], max_chars=None, key='guidance_input')
 
     # Generate text button
     if st.button('Generate Text', key='button1'):  # Add a unique key for the button
@@ -42,11 +39,15 @@ def app():
     # Section 2: Check Guidance
     if st.session_state["response"]:
         st.header('Check Guidance')
+        # Dropdown for Guidance
+        st.subheader('Guidance')
+        guidance_option = st.selectbox('Select a guidance', list(GUIDANCE_OPTIONS.keys()), key='guidance')
+        st.session_state["guidance_input"] = st.text_area('What does it need to comply with (from Section 1)?', value=GUIDANCE_OPTIONS[guidance_option], max_chars=None, key='guidance_input')
 
         if st.button('Check Compliance', key='button2'):  # Add a unique key for the button
-            if guidance_input:
+            if st.session_state["guidance_input"]:
                 messages = [
-                    {"role": "system", "content": f"You are provided with the following guidance for what a message should comply with {guidance_input}. You need to do the following: 1) Create clear assessment categories based on the criteria, 2) Analyse the text provided against it, 3) Score the text against each category on 1-5 scale (1=poor, 5=excellent),4) Provide short commentary against each category, 5) On the very top of your assessment give an overall score and a short assessment of the message versus guidance. Format all of this in a markdown table"},
+                    {"role": "system", "content": f"You are provided with the following guidance for what a message should comply with {st.session_state['guidance_input']}. You need to do the following: 1) Create clear assessment categories based on the criteria, 2) Analyse the text provided against it, 3) Score the text against each category on 1-5 scale (1=poor, 5=excellent),4) Provide short commentary against each category, 5) On the very top of your assessment give an overall score and a short assessment of the message versus guidance. Format all of this in a markdown table"},
                     {"role": "user", "content": f"Analyse the following text: {st.session_state['response']}"},
                 ]
                 response = openai_utils.send_request_to_openai(messages)
