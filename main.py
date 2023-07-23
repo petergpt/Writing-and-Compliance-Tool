5a0953830882a139cedd4c8d8756db33870915af
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 21:03:49 +0000

    try fix again

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from multiapp import MultiApp
-from generate_text import app as generate_text_app
-from check_guidance import app as check_guidance_app
-from test_persona import app as test_persona_app
+from pages.generate_text import app as generate_text_app
+from pages.check_guidance import app as check_guidance_app
+from pages.test_persona import app as test_persona_app
 
 app = MultiApp()
 
