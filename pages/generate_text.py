44127340c20fc6f70f8b05a2aa83ee51c1fc7c29
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 21:20:54 +0000

    try again

--- pages/generate_text.py
+++ pages/generate_text.py
@@ -9,18 +9,19 @@ def app():
 
     st.subheader('Tone of Voice')
     tone_option = st.selectbox('Select a tone', list(TONE_OF_VOICE_OPTIONS.keys()), key='tone')
+    
+    tone_input = TONE_OF_VOICE_OPTIONS[tone_option] # Use the chosen option directly
+    st.text_area('What style should be written in?', value=tone_input, max_chars=None, key='tone_input')
 
     if st.button('Generate Text', key='button1'):
-        if text_input:
-            tone_input = TONE_OF_VOICE_OPTIONS[tone_option]
+        if text_input and tone_input:
             with st.spinner('Generating text...'):
                 messages = [
                     {"role": "system", "content": f"You have two options: 1) You are given instructions to generate a certain bit of text OR 2) You are provided with a bit of text to re-write. Never list both options, only carry out one. For both, please follow this guidance: {tone_input}."},
                     {"role": "user", "content": f"{text_input}"},
                 ]
                 response = send_request_to_openai(messages)
-                st.session_state["response"] = response['choices'][0]['message']['content']
-                st.session_state["generated_text"] = st.session_state["response"]
-                
-    st.text_area('What style should be written in?', value=TONE_OF_VOICE_OPTIONS[tone_option], max_chars=None, key='tone_input')
+                generated_text = response['choices'][0]['message']['content']
+                st.session_state["generated_text"] = generated_text
+
     st.text_area('Your text will appear here', value=st.session_state.get("generated_text", ""), max_chars=None, key=None)
