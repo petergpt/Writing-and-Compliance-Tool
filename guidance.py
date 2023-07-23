2d44d90a2e3505c6d0cfa7aead6f71a6ec7890fa
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 08:08:10 +0000

    added tone of voice and guidance options

--- guidance.py
+++ guidance.py
@@ -1,6 +1,23 @@
 # Dictionary mapping dropdown options to corresponding text
 GUIDANCE_OPTIONS = {
-    "Option 1": "Text for Option 1",
+    "Consumer Duty": """
+1. Use clear, plain language and avoid jargon. Explain any technical terms.
+2. Ensure the communication provides key information needed for informed, timely decisions in customers' interests.
+3. Adopt a professional but customer-focused tone. Avoid overly formal language.
+4. Prominently highlight important details through layout, visual design, headings etc.
+5. Tailor level of detail and complexity to the needs and characteristics of the target audience.
+6. Address diverse needs of vulnerable customers where possible through alternate formats etc.
+7. Explain required actions, consequences and timeframes clearly. Make calls to action stand out.
+8. Provide context on how the communication fits with others from the firm.
+9. Give adequate time for decisions where applicable. Choose appropriate channels.
+10. Use active voice and avoid ambiguous phrases. Address the customer directly.
+11. Use visuals, scenarios, examples etc to aid understanding where appropriate.
+12. Summarize key features, benefits and risks in a simple, easy-to-digest format.
+13. Give prominence to risks, limitations or exclusions; do not hide in small print.
+14. Enable customer to save, print or download important information.
+15. Ensure consistency with other materials in customer journey including verbal.
+16. Provide access to communication through multiple channels if possible.
+""",
     "Option 2": "Text for Option 2",
     # Add more options here
 }
--- tone_of_voice.py
+++ tone_of_voice.py
@@ -1,6 +1,31 @@
 # Dictionary mapping dropdown options to corresponding text
 TONE_OF_VOICE_OPTIONS = {
-    "Option 1": "Text for Option 1",
+    "Sainsbury's": """
+
+When writing for Sainsbury's:
+
+1. Use a warm, friendly tone. Avoid sounding corporate or formal. Make the customer feel like they're interacting with a real person.
+
+For example: "Start shopping with us" instead of "Commence your shop with Sainsbury's"
+
+2. Use conversational language. Write how you would speak to someone face-to-face.
+
+For example: "It's almost time to pick up your order" instead of "It is almost time to pick up your order"
+
+3. Address the customer directly as "you" and "your". Don't use third person like "the customer".
+
+For example: "Set up your account details" rather than "Set up customer account details"
+
+4. Break up long sections of text with subheadings. Make it easy to scan and find information. Focus on the benefits to the customer. Explain why they should take an action.
+
+For example:"Register for an online account to order your favorites in a few clicks"
+
+5. Use exclamation points sparingly. Avoid over-excited tone. Occasionally drop in lively words and phrases, but don't overdo it. Stay natural.
+
+For example: "Grab extra savings when you order online"
+
+Don't sacrifice clarity for liveliness. Make sure key messages come through.
+""",
     "Option 2": "Text for Option 2",
     # Add more options here
 }
