8d8478018756ca3e4df8744a252f315c8d68f597
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 20:51:47 +0000

    fix main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import streamlit as st
-import app_generate_text
-import app_check_guidance
-import app_test_persona
+import generate_text, check_guidance, test_persona
 
+PAGES = {
+    "Generate Text": generate_text,
+    "Check Guidance": check_guidance,
+    "Test Persona Perception": test_persona
+}
 
 def main():
     st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Go to", ['Generate Text', 'Check Guidance', 'Test Persona'])
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 
-    if selection == 'Generate Text':
-        app_generate_text.app()
-    elif selection == 'Check Guidance':
-        app_check_guidance.app()
-    elif selection == 'Test Persona':
-        app_test_persona.app()
+    page = PAGES[selection]
 
+    with st.spinner(f"Loading {selection} ..."):
+        page.app()
 
 if __name__ == "__main__":
     main()
