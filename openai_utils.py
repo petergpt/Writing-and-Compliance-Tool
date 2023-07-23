aeb5bfc134be7e4ee8b8449182742be5d61125e5
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 20:19:15 +0000

    fix key 3x

--- openai_utils.py
+++ openai_utils.py
@@ -1,13 +1,8 @@
 import os
 import openai
 
-openai.organization = "org-RgaR5omwSOHKfgH8FjtxRDis"
-
-#OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
-#openai.api_key = "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"
-
-openai.api_key = OPENAI_API_KEY
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+openai.api_key = OPENAI_API_KEY
 
 def send_request_to_openai(messages):
     
