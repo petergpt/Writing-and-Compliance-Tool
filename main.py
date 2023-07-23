994d175227c34590098b3ed197377c105f223689
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 21:02:21 +0000

    fix imports in main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import streamlit as st
 from multiapp import MultiApp
-from pages import generate_text, check_guidance, test_persona
+from generate_text import app as generate_text_app
+from check_guidance import app as check_guidance_app
+from test_persona import app as test_persona_app
 
 app = MultiApp()
 
-st.sidebar.title('Navigation')
+app.add_app("Generate Text", generate_text_app)
+app.add_app("Check Guidance", check_guidance_app)
+app.add_app("Test Persona", test_persona_app)
 
-# Add all your application here
-app.add_app("Generate Text", generate_text.app)
-app.add_app("Check Guidance", check_guidance.app)
-app.add_app("Test Persona", test_persona.app)
-
-# The main app
 app.run()
