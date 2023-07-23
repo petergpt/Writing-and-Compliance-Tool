073d9d5eae90eb2333e00bde6d6576f97e3097f3
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 20:52:33 +0000

    revert to old main.py

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import streamlit as st
-import generate_text, check_guidance, test_persona
+import page
 
-PAGES = {
-    "Generate Text": generate_text,
-    "Check Guidance": check_guidance,
-    "Test Persona Perception": test_persona
-}
-
-def main():
-    st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-
-    page = PAGES[selection]
-
-    with st.spinner(f"Loading {selection} ..."):
-        page.app()
-
-if __name__ == "__main__":
-    main()
+# Call the app function directly
+page.app()
