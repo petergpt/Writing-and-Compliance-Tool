e1c3ad6956624732e74edfb8785d2ad90ee0a048
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 21:05:15 +0000

    another try

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from pages.generate_text import app as generate_text_app
 from pages.check_guidance import app as check_guidance_app
 from pages.test_persona import app as test_persona_app
 
+# Create an instance of the app
 app = MultiApp()
 
+# Add all your application here
 app.add_app("Generate Text", generate_text_app)
 app.add_app("Check Guidance", check_guidance_app)
 app.add_app("Test Persona", test_persona_app)
 
+# The main app
 app.run()
