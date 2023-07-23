b72316c765048db82129e353b62d1c7ab776a53e
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 22:32:18 +0000

    try another one

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import streamlit as st
 import page
 
-page.run()
+def main():
+    st.title("OpenAI GPT-4 Text Generation Interface")
+    page.generate_text()
+    page.check_guidance()
+    page.test_persona_perception()
+
+if __name__ == "__main__":
+    main()
