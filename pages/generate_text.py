db96ce2e95d8296d6f3fdfeb1ee1fdd86806ccfd
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-23 21:18:16 +0000

    try fixing again

--- pages/generate_text.py
+++ pages/generate_text.py
@@ -9,19 +9,18 @@ def app():
 
     st.subheader('Tone of Voice')
     tone_option = st.selectbox('Select a tone', list(TONE_OF_VOICE_OPTIONS.keys()), key='tone')
-    
-    # Change here: initialize tone_input from session state if it exists
-    tone_input = st.text_area('What style should be written in?', value=st.session_state.get("tone_input", TONE_OF_VOICE_OPTIONS[tone_option]), max_chars=None, key='tone_input')
 
     if st.button('Generate Text', key='button1'):
-        if text_input and tone_input:
+        if text_input:
             with st.spinner('Generating text...'):
                 messages = [
-                    {"role": "system", "content": f"You have two options: 1) You are given instructions to generate a certain bit of text OR 2) You are provided with a bit of text to re-write. Never list both options, only carry out one. For both, please follow this guidance: {tone_input}."},
+                    {"role": "system", "content": f"You have two options: 1) You are given instructions to generate a certain bit of text OR 2) You are provided with a bit of text to re-write. Never list both options, only carry out one. For both, please follow this guidance: {TONE_OF_VOICE_OPTIONS[tone_option]}."},
                     {"role": "user", "content": f"{text_input}"},
                 ]
                 response = send_request_to_openai(messages)
                 st.session_state["response"] = response['choices'][0]['message']['content']
                 st.session_state["generated_text"] = st.session_state["response"]
+                
+    tone_input = st.text_area('What style should be written in?', value=st.session_state.get("tone_input", TONE_OF_VOICE_OPTIONS[tone_option]), max_chars=None, key='tone_input')
 
     st.text_area('Your text will appear here', value=st.session_state.get("generated_text", ""), max_chars=None, key=None)
