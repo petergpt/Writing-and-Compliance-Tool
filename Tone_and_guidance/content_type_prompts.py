409ebf2a4b732781c413bc077cf7a27bf0f3275b
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-26 21:35:39 +0000

    added extra functionality to generate

---
+++ Tone_and_guidance/content_type_prompts.py
@@ -0,0 +1,6 @@
+CONTENT_TYPE_PROMPTS = {
+    "Marketing Copy": "Your system prompt for marketing copy",
+    "Job Description": "Your system prompt for job description",
+    "Internal Communication": "Your system prompt for internal communication",
+    # Add more as needed
+}
--- page.py
+++ page.py
@@ -5,13 +5,29 @@ from Tone_and_guidance.tone_of_voice import TONE_OF_VOICE_OPTIONS
 from Tone_and_guidance.guidance import GUIDANCE_OPTIONS
 from Tone_and_guidance.questions import QUESTIONS_OPTIONS
 from Tone_and_guidance.personas import PERSONAS_OPTIONS
+from content_type_prompts import CONTENT_TYPE_PROMPTS
+
+def main():
+    st.sidebar.title('Navigation')
+    selection = st.sidebar.radio("Go to", ['Generate Text', 'Check Guidance', 'Test Persona Perception'])
+
+    if selection == 'Generate Text':
+        generate_text()
+    elif selection == 'Check Guidance':
+        check_guidance()
+    elif selection == 'Test Persona Perception':
+        test_persona_perception()
 
 def generate_text():
     st.header('Generate Text')
 
-    # Initialize session state if it doesn't exist
-    if "generated_text" not in st.session_state:
-        st.session_state["generated_text"] = ""
+    # Choose content type
+    content_type = st.selectbox('Select a content type', list(CONTENT_TYPE_PROMPTS.keys()), key='content_type')
+    prompt = CONTENT_TYPE_PROMPTS[content_type]
+
+    # Editable system prompt
+    if st.checkbox('Show/hide system prompt'):
+        prompt = st.text_area('Edit the system prompt if you want', value=prompt, key='system_prompt')
 
     text_input = st.text_area('What do you want to write?', value='', max_chars=10000)
 
@@ -23,7 +39,7 @@ def generate_text():
         if text_input and tone_input:
             with st.spinner('Generating text...'):
                 messages = [
-                    {"role": "system", "content": f"There are 2 options, 1) You are given instructions to generate a certain bit of text OR 2) You are provided with a bit of text to re-write in the tone of voice provided. Never do both options, only carry out one. For both, please follow this guidance: {tone_input}."},
+                    {"role": "system", "content": f"{prompt}"},
                     {"role": "user", "content": f"{text_input}"},
                 ]
                 response = openai_utils.send_request_to_openai(messages)
@@ -31,57 +47,61 @@ def generate_text():
 
     st.text_area('Your text will appear here', value=st.session_state["generated_text"], max_chars=None, key=None)
 
-
 def check_guidance():
-    if "generated_text" in st.session_state and st.session_state["generated_text"]:
-        st.header('Check Guidance')
+    st.header('Check Guidance')
 
-        st.subheader('Guidance')
-        guidance_option = st.selectbox('Select a guidance', list(GUIDANCE_OPTIONS.keys()), key='guidance')
-        st.session_state["guidance_input"] = st.text_area('What does it need to comply with (from Section 1)?', value=GUIDANCE_OPTIONS[guidance_option], max_chars=None, key='guidance_input_widget')
+    # Editable system prompt
+    default_prompt = "You are provided with the following guidance for what a message should comply..."
+    if st.checkbox('Show/hide system prompt'):
+        default_prompt = st.text_area('Edit the system prompt if you want', value=default_prompt, key='system_prompt_check_guidance')
 
-        if st.button('Check Compliance', key='button2'):  
-            if st.session_state["guidance_input"]:
-                with st.spinner('Checking for compliance...'):
-                    messages = [
-                        {"role": "system", "content": f"You are provided with the following guidance for what a message should comply with {st.session_state['guidance_input']}. You need to do the following: 1) Create clear assessment categories based on the criteria, 2) Analyse the text provided against it, 3) Score the text against each category on a 1-5 scale (1=poor, 5=excellent),4) Provide short commentary against each category, 5) On the very top of your assessment give an overall score and a short assessment of the message versus guidance. Format all of this in a markdown table"},
-                        {"role": "user", "content": f"Analyse the following text: {st.session_state['generated_text']}"},
-                    ]
-                    response = openai_utils.send_request_to_openai(messages)
-                    st.session_state["result"] = response['choices'][0]['message']['content']  # Store the content in the session state
+    st.subheader('Guidance')
+    guidance_option = st.selectbox('Select a guidance', list(GUIDANCE_OPTIONS.keys()), key='guidance')
+    st.session_state["guidance_input"] = st.text_area('What does it need to comply with (from Section 1)?', value=GUIDANCE_OPTIONS[guidance_option], max_chars=None, key='guidance_input_widget')
 
-            else:
-                st.warning('Please enter guidance and ensure text is generated in Section 1')
+    if st.button('Check Compliance', key='button2'):  
+        if st.session_state["guidance_input"]:
+            with st.spinner('Checking for compliance...'):
+                messages = [
+                    {"role": "system", "content": f"{default_prompt}"},
+                    {"role": "user", "content": f"Analyse the following text: {st.session_state['generated_text']}"},
+                ]
+                response = openai_utils.send_request_to_openai(messages)
+                st.session_state["result"] = response['choices'][0]['message']['content']  # Store the content in the session state
+
+        else:
+            st.warning('Please enter guidance and ensure text is generated in Section 1')
+
         if "result" in st.session_state:
             st.markdown(st.session_state["result"])  # Display the stored result
-    # else:
-    #     st.warning('Please generate text first')
-
-
 
 def test_persona_perception():
-    if "generated_text" in st.session_state and st.session_state["generated_text"] and "guidance_input" in st.session_state and st.session_state["guidance_input"]:
-        st.header('Test Persona Perception')
+    st.header('Test Persona Perception')
 
-        st.subheader('Questions')
-        questions_option = st.selectbox('Select a set of questions', list(QUESTIONS_OPTIONS.keys()), key='questions')
-        st.session_state["questions_input"] = st.text_area('The following questions will be answered by the persona', value=QUESTIONS_OPTIONS[questions_option], max_chars=None, key='questions_input_widget')
+    # Editable system prompt
+    default_prompt = "You will fully embody a persona of a {persona_option} and answer the following questions based on the text..."
+    if st.checkbox('Show/hide system prompt'):
+        default_prompt = st.text_area('Edit the system prompt if you want', value=default_prompt, key='system_prompt_test_persona')
 
-        st.subheader('Persona')
-        persona_option = st.selectbox('Select a persona', list(PERSONAS_OPTIONS.keys()), key='persona')
-        st.session_state["persona_input"] = st.text_area('The following persona will answer the questions', value=PERSONAS_OPTIONS[persona_option], max_chars=None, key='persona_input_widget')
+    st.subheader('Questions')
+    questions_option = st.selectbox('Select a set of questions', list(QUESTIONS_OPTIONS.keys()), key='questions')
+    st.session_state["questions_input"] = st.text_area('The following questions will be answered by the persona', value=QUESTIONS_OPTIONS[questions_option], max_chars=None, key='questions_input_widget')
 
-        if st.button('Test Persona', key='button3'):  
-            if st.session_state["questions_input"] and st.session_state["persona_input"]:
-                with st.spinner('Testing persona perception...'):
-                    messages = [
-                        {"role": "system", "content": f"You will fully embody a persona of a {persona_option} and answer the following questions based on the text. You must answer questions in line with what the persona would have answered after reading the text that will be provided to you. Answer the following questions: {st.session_state['questions_input']}. Format your answers in a markdown table, columns: #, Question, Answer, Commentary. At the end provide an Overall Assessment giving a score of 1-5 (1=poor, 5=excellent) and providng commentary."},
-                        {"role": "user", "content": f"Read the following text: {st.session_state['generated_text']}"},
-                    ]
-                    response = openai_utils.send_request_to_openai(messages)
-                    st.markdown(response['choices'][0]['message']['content'])
-            else:
-                st.warning('Please enter questions and persona, and ensure text is generated and guidance is entered')
-    # else:
-    #     st.warning('Please generate text and check guidance first')
+    st.subheader('Persona')
+    persona_option = st.selectbox('Select a persona', list(PERSONAS_OPTIONS.keys()), key='persona')
+    st.session_state["persona_input"] = st.text_area('The following persona will answer the questions', value=PERSONAS_OPTIONS[persona_option], max_chars=None, key='persona_input_widget')
 
+    if st.button('Test Persona', key='button3'):  
+        if st.session_state["questions_input"] and st.session_state["persona_input"]:
+            with st.spinner('Testing persona perception...'):
+                messages = [
+                    {"role": "system", "content": f"{default_prompt}"},
+                    {"role": "user", "content": f"Read the following text: {st.session_state['generated_text']}"},
+                ]
+                response = openai_utils.send_request_to_openai(messages)
+                st.markdown(response['choices'][0]['message']['content'])
+        else:
+            st.warning('Please enter questions and persona, and ensure text is generated and guidance is entered')
+
+if __name__ == "__main__":
+    main()
