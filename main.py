b6b96332ec3d703fb7a5db78df41d5bfa049c411
petergpt <130558610+petergpt@users.noreply.github.com>
2023-07-27 08:23:54 +0000

    change main to show navigation

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import streamlit as st
 import page
 from emoji import emojize
 
-
 def main():
     st.title("Quentin 🤖 - AI-based Text Generator & Checker")
-    page.generate_text()
-    page.check_guidance()
-    page.test_persona_perception()
+    page.main()
 
 if __name__ == "__main__":
     main()
