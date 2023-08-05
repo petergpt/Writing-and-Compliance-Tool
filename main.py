48f884a772fe64ca6c3308a67b96f9cd9b87967c
petergpt <130558610+petergpt@users.noreply.github.com>
2023-08-05 21:17:53 +0000

    add in db ref

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import page
+from database_utils import setup_database
 
 def main():
+    setup_database()  # Initialize the database
     st.title("Quentin 🤖 - AI-based Text Generator & Checker")
     page.main()
 
