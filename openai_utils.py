1b523825c6ad64873a33605f630d154e4faabdd9
petergpt <130558610+petergpt@users.noreply.github.com>
2023-11-22 20:29:16 +0000

    Updated API structure

--- openai_utils.py
+++ openai_utils.py
@@ -5,12 +5,14 @@ OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 openai.api_key = OPENAI_API_KEY
 
 def send_request_to_openai(messages):
-    
-    response = openai.ChatCompletion.create(
-      model="gpt-4",
-      messages=messages,
-      temperature=0.8,
-      max_tokens=2000  
-    )
+    client = openai.OpenAI()
 
-    return response
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=messages,
+        temperature=0.7,
+        max_tokens=2000
+      )
+
+    return response.choices[0].message
+
